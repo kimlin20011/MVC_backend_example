3cc8313c36ee52657b1b09a0c6263300c2cbf7c0
ShunLin <kimlin20011@gmail.com>
2018-09-24 21:55:31 +0800

    20180924

---
+++ member/routes/member.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+	console.log(req.body.test)
+  //res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
